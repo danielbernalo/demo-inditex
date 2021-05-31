82c5e525dd1f27d2b6744f12f5aa8e22e6995002
Daniel Bernal <daniel.bernal@asesormasmovil.es>
2021-05-31 15:35:21 -0300

    add common module, this module shared entity and repository with others

---
+++ prices-worker/src/main/java/com/bernal/inditex/prices/worker/repository/PricesRepository.java
@@ -0,0 +1,8 @@
+package com.bernal.inditex.prices.worker.repository;
+
+import com.bernal.inditex.prices.worker.domain.Price;
+import org.springframework.data.repository.CrudRepository;
+
+public class PricesRepository extends Crud<Price> {
+
+}
