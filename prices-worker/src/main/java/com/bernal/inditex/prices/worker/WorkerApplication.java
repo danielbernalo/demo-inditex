c9f949f6fe7d366442d8e9057d3dffd2b482c0c1
Daniel Bernal <daniel.bernal@asesormasmovil.es>
2021-05-31 09:25:51 -0300

    Initial Commit

---
+++ prices-worker/src/main/java/com/bernal/inditex/prices/worker/WorkerApplication.java
@@ -0,0 +1,20 @@
+package com.bernal.inditex.prices.worker;
+
+import com.bernal.inditex.prices.worker.jobs.WorkerRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WorkerApplication {
+	@Bean
+	public CommandLineRunner applicationRunner() {
+		return new WorkerRunner();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerApplication.class, args);
+	}
+
+}
